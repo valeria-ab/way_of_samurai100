41026d2bc42c9e471c59c78ca93de79a65ef80e1
valeria-ab <lera.magnum@gmail.com>
2021-08-12 16:37:48 +0300

    add existing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {Route} from "react-router-dom"
+import MyFriends from "./components/MyFriends/MyFriends";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    return (
+        <div className='app-wrapper'>
+            <HeaderContainer/>
+            <Navbar/>
+            <div className={'content-wrapper'}>
+                <Route path={'/login'} render={() =>   <Login />}/>
+                <Route path={'/profile/:userId?'} render={() =>   <ProfileContainer  />}/>
+                <Route path={'/dialogs'} render={() =>   <DialogsContainer />}/>
+                <Route path={'/friends'} render={() =>   <MyFriends />}/>
+                <Route path={'/users'} render={() =>   <UsersContainer />}/>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import store from "./redux/redux-store";
+import {Provider} from "react-redux";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store={store} >
+                <App />
+            </Provider>
+
+        </BrowserRouter>, document.getElementById('root'));
+
+
+
+
+
+
+
